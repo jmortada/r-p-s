c9dd5f44216cacae03c33384dfe7607e8363b83a
jmortada <158744391+jmortada@users.noreply.github.com>
2025-02-14 18:08:25 +0200

    wrote a function to play one round of rock paper scissors, reurn if it is a win or a lose and increasing the score of the winner

--- rps.js
+++ rps.js
@@ -13,3 +13,29 @@ let message = "enter rock paper or scissors";
 function getHumanChoice () {
     return prompt(message);
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound (humanChoice , computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice)
+        console.log("it is a tie");
+    return;
+
+    let winCon = {
+        paper : "rock",
+        rock : "scissors",
+        scissors : "paper"
+    }
+
+    if (winCon[humanChoice] === computerChoice){
+        console.log("You Win! " +humanChoice+ " beats " +computerChoice );
+        humanScore++;
+    }
+    else{
+        console.log("You Lose! " +computerChoice+ " beats " +humanChoice);
+        computerScore++;
+    }
+}
